bf7eeff56e3e1d2f3abfdea05b6369acbc533d16
EmmanuelZambrano <ezambran@hawk.iit.edu>
2016-02-19 14:52:07 -0600

    added js to show message when name is focused

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 var input = document.getElementsByTagName('input')[1];
 var inp = document.getElementById("tryout");
 var para = document.createElement("P");
-var t = document.createTextNode("You must enter your IIT email.");
+var p = document.createElement("P");
+var t = document.createTextNode("Please enter your IIT email address.");
+var text = document.createTextNode("Enter your name.");
+var name= document.getElementById("name");
 
-input.onfocus = function NameHelp(){
+name.onfocus = function NameHelp(){
+	if (this.id == "name"){
+		p.appendChild(text);
+		inp.insertBefore(p, inp.childNodes[4]);
+	}
+}
+name.onblur = function RemoveName(){
+	inp.removeChild(p);
+}
+
+input.onfocus = function EmailHelp(){
 	if (this.id == "email"){
 		para.appendChild(t); 
   	inp.insertBefore(para, inp.childNodes[8]);
